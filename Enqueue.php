b76a7f0f96e2521441dfc67bcab9a79931233e3c
kermage <genealyson.torcende@gmail.com>
2020-03-29 11:33:39 +0800

    do the async and defer of scripts

--- Enqueue.php
+++ Enqueue.php
@@ -11,6 +11,36 @@ namespace ThemePlate;
 
 class Enqueue {
 
-	public static function init() {}
+	private static $storage = array();
+
+	public static function init() {
+
+		global $wp_scripts;
+
+		foreach ( $wp_scripts->registered as $handle => $dependency ) {
+			if ( ! empty( $dependency->extra['async'] ) ) {
+
+				self::$storage[ $handle ] = 'async';
+			} elseif ( ! empty( $dependency->extra['defer'] ) ) {
+				self::$storage[ $handle ] = 'defer';
+			}
+		}
+
+		add_filter( 'script_loader_tag', array( Enqueue::class, 'hooker' ), 10, 2 );
+
+	}
+
+
+	public static function hooker( $tag, $handle ) {
+
+		if ( array_key_exists( $handle, self::$storage ) ) {
+			$attribute = self::$storage[ $handle ];
+
+			return str_replace( ' src', " $attribute src", $tag );
+		}
+
+		return $tag;
+
+	}
 
 }
