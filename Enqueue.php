b71ec56715cf47928efdbdee6d767e2b22d4db8d
kermage <genealyson.torcende@gmail.com>
2020-03-29 11:23:40 +0800

    Initial commit

---
+++ Enqueue.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Helper for registered dependencies
+ *
+ * @package ThemePlate
+ * @since 0.1.0
+ */
+
+namespace ThemePlate;
+
+class Enqueue {
+
+	public static function init() {}
+
+}
